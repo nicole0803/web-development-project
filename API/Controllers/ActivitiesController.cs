40d17b26805ebc34ba28ad05169d458409cd1436
nicole0803 <leenicole8399@gmail.com>
2025-09-10 13:20:47 +0800

    Initial Commit: Create .NET project from scratch, create API

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,35 @@
+using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseAPiController
+{
+    private readonly AppDbContext context;
+    public ActivitiesController(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+
+        if (activity == null) return NotFound();
+
+        return activity;
+
+     }
+
+
+}
